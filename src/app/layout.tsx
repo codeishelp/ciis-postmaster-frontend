72078c47ede077bccceb221575d3c811fbf08267
codeishelp <oacsurco@gmail.com>
2025-08-04 20:04:23 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { jetBrainsMono, titleFont } from "@/config";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "XII TEST",
+  title: "XII POSTMASTER",
   description: "Encuentro de egresados de la Escuela Profesional de Ingeniería en Informática y Sistemas",
 };
 
